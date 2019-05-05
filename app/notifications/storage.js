596b29c3113d428df5656f0a65f18ea172d66f0e
Lucas Andrade <lucasandrad@yandex.com>
2019-05-05 15:49:09 -0300

    Create edit notification method #2

--- app/notifications/storage.js
+++ app/notifications/storage.js
@@ -1,23 +1,37 @@
 var storage = window.localStorage;
 
 function createNotification(id, time, name) {
-  let notification = JSON.stringify({ id: id, time: time, name: name })
+  let notification = { id: id, time: time, name: name }
   let notifications = loadNotifications()
   notifications.push(notification)
   storage.setItem("notifications", JSON.stringify(notifications))
 }
 
 function getNotifications() {
-  var storeNoti = JSON.parse(localStorage.getItem("notifications"))
-  return storeNoti;
+  return loadNotifications()
+}
+
+function editNotification(id, time, name) {
+  let notifs = loadNotifications()
+  index = findNotification(id, notifs)
+  notifs[index] = JSON.stringify({ id: id, time: time, name: name })
+  storage.setItem("notifications", JSON.stringify(notifs))
 }
 
 function loadNotifications() {
-  let notfs = JSON.parse(localStorage.getItem("notifications"))
-  if(notfs === null || notfs === "null" || notfs === "[]") {
+  let notifs = JSON.parse(localStorage.getItem("notifications"))
+  if(notifs === null || notifs === "null" || notifs === "[]") {
     return []
   }
   else {
-    return notfs
+    return notifs
+  }
+}
+
+function findNotification(id, notifs) {
+  for(i in notifs) {
+    if(notifs[i].id == id) {
+      return parseInt(i)
+    }
   }
 }
